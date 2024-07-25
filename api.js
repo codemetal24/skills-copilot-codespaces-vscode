ce6b7b04165f006df843d0688d9fe7fd56c22fb9
codemetal24 <koldmetal@yahoo.com>
2024-07-25 14:37:36 -0700

    Update api.js

--- api.js
+++ api.js
@@ -1,1 +1,7 @@
+// Create a web API using express and JavaScript with routes for products and customers 
+// that return JSON data. The products and customers data can be hardcoded in the server.
+// The server should listen on port 3000.
+// The server should have a route that returns a 404 error for any other route.
+// The server should have a route that returns a 500 error for any other error.
+// The server should have a route that returns a 200 status for the root route.
 
